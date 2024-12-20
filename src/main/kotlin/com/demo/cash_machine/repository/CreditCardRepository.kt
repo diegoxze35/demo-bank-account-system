ce0185b13515609619409ac744d5c7f31aca4ff2
Emiliano <ediazh1900@alumno.ipn.mx>
2024-12-19 23:42:40 -0600

    Readme

--- src/main/kotlin/com/demo/cash_machine/repository/CreditCardRepository.kt
+++ src/main/kotlin/com/demo/cash_machine/repository/CreditCardRepository.kt
@@ -2,5 +2,15 @@ package com.demo.cash_machine.repository
 
 import com.demo.cash_machine.repository.entity.CreditCard
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.transaction.annotation.Transactional
 
-interface CreditCardRepository : JpaRepository<CreditCard, Int>
+interface CreditCardRepository : JpaRepository<CreditCard, Int>{
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE credit_card c SET c.credit_balance = c.credit_balance + :amount WHERE c.card_id = :cardId", nativeQuery = true)
+    fun withdrawCreditCard(@Param("cardId") cardId: Int, @Param("amount") amount: Double)
+}
--- src/main/kotlin/com/demo/cash_machine/service/WithdrawalService.kt
+++ src/main/kotlin/com/demo/cash_machine/service/WithdrawalService.kt
@@ -3,16 +3,17 @@ package com.demo.cash_machine.service
 import com.demo.cash_machine.model.response.WithdrawalResponse
 import com.demo.cash_machine.repository.AccountRepository
 import com.demo.cash_machine.repository.CardRepository
+import com.demo.cash_machine.repository.CreditCardRepository
 import com.demo.cash_machine.repository.DebitCardRepository
 import com.demo.cash_machine.repository.entity.CreditCard
 import com.demo.cash_machine.repository.entity.DebitCard
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class WithdrawalService(
     private val cardRepository: CardRepository,
     private val debitCardRepository: DebitCardRepository,
+    private val creditCardRepository: CreditCardRepository,
     private val accountRepository: AccountRepository
 ) {
 
@@ -22,11 +23,19 @@ class WithdrawalService(
         if (card is DebitCard) {
             if (card.balance < amount) return WithdrawalResponse("Not enough money on your debit card")
             if (card.account.sumWithdrawal + amount > 50000) return WithdrawalResponse("Withdrawal limit exceeded")
-            if (card.number.substring(0, 3) == "2222")
-                debitCardRepository.withdrawMoneyFromCard(card.id, amount)
+            if (card.number.substring(0..3) == "2222")
+                debitCardRepository.withdrawDebitCard(card.id, amount)
             else
-                debitCardRepository.withdrawMoneyFromCard(card.id, amount + 50)
-        } else if (card is CreditCard) return WithdrawalResponse("Credit card can't be used for withdrawal")
+                debitCardRepository.withdrawDebitCard(card.id, amount + 50)
+        } else if (card is CreditCard) {
+            if (card.number.substring(0..3) == "2222") {
+                if (amount + card.creditBalance > card.creditLimit) return WithdrawalResponse("Not enough money on your credit card")
+                creditCardRepository.withdrawCreditCard(card.id, amount)
+            } else {
+                if (amount + card.creditBalance + 50 > card.creditLimit) return WithdrawalResponse("Not enough money on your credit card")
+                creditCardRepository.withdrawCreditCard(card.id, amount + 50)
+            }
+        }
 
         accountRepository.updateSumWithdrawal(card.account.id, amount)
         return WithdrawalResponse("successful")
